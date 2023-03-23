9b977f320f65f79239f410e61f43d713712828e8
paketchino <chansgorn@gmail.com>
2023-03-23 18:02:39 +0300

    Task 1.1.5 fix

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -6,7 +6,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.logging.Logger;
@@ -20,15 +19,17 @@ public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
     }
 
-    private <T> T tx(final Function<Session, T> command) {
-        final Session session = sf.openSession();
-        final Transaction tx = session.beginTransaction();
+    private <T> T tx(Function<Session, T> command) {
+        Session session = sf.openSession();
+        Transaction tx = session.beginTransaction();
         try {
             T rsl = command.apply(session);
             tx.commit();
             return rsl;
-        } catch (final Exception e) {
+        } catch (Exception e) {
             session.getTransaction().rollback();
+            String rsl = String.format("Во время выполнения транзакция что-то пошло не так %s", e.getMessage());
+            logger.warning(rsl);
             throw e;
         } finally {
             session.close();
@@ -37,6 +38,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        logger.info("Выполняется команда добавления таблицы users");
         Session session = sf.openSession();
         session.beginTransaction();
         session.createSQLQuery(
@@ -46,60 +48,50 @@ public class UserDaoHibernateImpl implements UserDao {
                         "last_name text not null," +
                         "age tinyint not null)");
         session.getTransaction().commit();
+        logger.info("Таблица была успешно создана");
         session.close();
     }
 
     @Override
     public void dropUsersTable() {
+        logger.info("Выполняется команда удаления таблицы users");
         Session session = sf.openSession();
         session.beginTransaction();
         session.createSQLQuery("drop table if exists users");
         session.getTransaction().commit();
+        logger.info("Таблица была успешно удалена");
         session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        logger.info("Выполняется команда добавления пользователя");
         User user = new User();
         this.tx(session -> {
             user.setName(name);
             user.setLastName(lastName);
             user.setAge(age);
             session.save(user);
+            logger.info("Добавления пользователя успешно завершено");
             return user;
         });
     }
 
     @Override
     public void removeUserById(long id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("from users as u where u.id =:id");
-        session.getTransaction().commit();
-        session.close();
+        logger.info("Выполняется команда удаление пользователя по id");
+        tx(session -> session.createQuery("from users as u where u.id =:id"));
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> list = new ArrayList<>();
-        Session session = sf.openSession();
-        session.beginTransaction();
-        list = session.createQuery("from users").list();
-        session.getTransaction().commit();
-        session.close();
-        return list;
+        logger.info("Выполняется команда поиска всех пользователей");
+        return (List<User>) tx(session -> session.createQuery("from users").getResultList());
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = sf.openSession();
-        logger.info("Открывается транзакция по удалению всех пользователей");
-        session.beginTransaction();
-        session.createQuery("delete from users").executeUpdate();
-        logger.info("Выполняется удалению");
-        session.getTransaction().commit();
-        logger.info("Удаление выполнено");
-        session.close();
-        logger.info("Транзакция закрыта");
+        logger.info("Выполняется команда удаления всех пользователей");
+        tx(session -> session.createQuery("delete from users").executeUpdate());
     }
 }
